f90631c8ae6b294bbfe804924d38a7696109794a
aetonsi <18366087+aetonsi@users.noreply.github.com>
2022-08-30 15:20:14 +0200

    add file

---
+++ InputBox.psm1
@@ -0,0 +1,14 @@
+﻿
+Add-Type -AssemblyName Microsoft.VisualBasic
+
+function Get-InputBox {
+    param(
+      [Parameter(Mandatory=$true)] [string] $title,
+      [Parameter(Mandatory=$false)] [string] $msg = ""
+    )
+
+    return [Microsoft.VisualBasic.Interaction]::InputBox($msg, $title)
+}
+
+
+Export-ModuleMember -Function *
